d0a3473c2ab14dc693b7488029135533f57527e8
Jazeye <jaseelp394@gmail.com>
2024-01-27 18:59:27 +0530

    fix the path error

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/*.{html,js}"],
+  content: ["./Personal-Portfolio/*.{html,js}"],
   theme: {
     extend: {},
   },
