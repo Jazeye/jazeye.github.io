98de40dd668f5549794be6fe79d1b6a46530c63f
Jazeye <jaseelp394@gmail.com>
2024-02-09 23:59:01 +0530

    added navbar icon animation in js also icon added

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,19 @@
+//========toggle icon navbar=========//
+// JavaScript
+let menuIcon = document.querySelector("#menu-icon");
+let navbar = document.querySelector(".navbar");
+
+menuIcon.onclick = () => {
+  menuIcon.classList.toggle("active");
+  navbar.classList.toggle("active");
+
+  if (menuIcon.classList.contains("active")) {
+    menuIcon.src = "./src/icons/x-arrow.png";
+  } else {
+    menuIcon.src = "./src/icons/menu-icon.svg";
+  }
+};
+
 //========active scroll link=========//
 let sections = document.querySelectorAll("section");
 let navlinks = document.querySelectorAll("header nav a");
@@ -14,13 +30,24 @@ window.onscroll = () => {
         links.classList.remove("active");
       });
 
-      let activeNavlink = document.querySelector("header nav a[href='#" + id + "']");
+      let activeNavlink = document.querySelector(
+        "header nav a[href='#" + id + "']"
+      );
       if (activeNavlink) {
         activeNavlink.classList.add("active");
       }
     }
   });
   //========navbar sticky=========//
-  let header = document.querySelector('header');
-  header.classList.toggle('sticky', window.scrollY > 100);
+  let header = document.querySelector("header");
+  header.classList.toggle("sticky", window.scrollY > 100);
+
+  //========remove toggle icon when navbar click=========//
+  menuIcon.classList.remove("active");
+  navbar.classList.remove("active");
+     if (menuIcon.classList.contains("active")) {
+        menuIcon.src = "./src/icons/x-arrow.png";
+     } else {
+        menuIcon.src = "./src/icons/menu-icon.svg";
+     }
 };
